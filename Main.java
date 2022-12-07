8d5d379803398c807f551e07d68a1bb825d863a0
AnwaarAlbusaidi <AnwaarAlbusaidi5@gmail.com>
2022-12-07 07:16:46 -0800

    add Hello world program

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main 
+{
+  public static void main(String[] args) 
+{
+    int x = 5;
+    System.out.print("Hello World    ");
+    System.out.println("Anwaar Al-Busaidi");
+    System.out.println(x);
+}
+}
---
+++ Test.java
@@ -0,0 +1,14 @@
+class Test {
+	public static void main (String[] args)
+	{
+		int userInput;
+		System.out.println("Select an action");
+		System.out.println("------------------");
+		System.out.println("1. Generate hash");
+		System.out.println("2. Compare hash");
+		System.out.println("3. Re-compute");
+		System.nextline(userInput);
+		System.out.println(userInput);
+		
+	}
+}
